511a6b03a4ac1d56b183ed367f358d6d545268a0
bakhtovarrizozoda <bakhtovarrizozoda@gmail.com>
2023-06-03 18:07:51 +0500

    first commit

---
+++ New folder/Work/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+var list = new List<Task>();
+list.Add(CountUpAsync());
+list.Add(CountDownAsync());
+await Task.WhenAll(list);
+
+async Task CountDownAsync()
+{
+    for(int i=10; i>=1; i--)
+    {
+        Console.WriteLine(($"Timer #1 : {i} seconds"));
+        await Task.Delay(1000);
+    }
+    Console.WriteLine(($"Timer #1 is completed")); 
+}
+async Task CountUpAsync()
+{
+    for(int i=1; i<=10; i++)
+    {
+        Console.WriteLine(($"Timer #2 : {i} seconds"));
+        await Task.Delay(1000);
+    }
+    Console.WriteLine(($"Timer #2 is completed")); 
+}
